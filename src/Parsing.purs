709480ad9bc499d6d6cde161efb8c24c6fcb956d
Ryan Mechelke <ryan@mechelke.us>
2022-12-04 15:01:29 -0600

    completed Day4

--- src/Parsing.purs
+++ src/Parsing.purs
@@ -21,7 +21,7 @@ linesFrom parser = split (Pattern "\n") >>> traverse parser
 -- | Takes two Parser functions and returns a Tuple of both results if
 -- | successful
 tupleParser :: forall x y. Parser x -> Parser y -> String -> Maybe (Tuple x y)
-tupleParser parseLeft parseRight = tupleParser' parseLeft (\_ -> parseRight)
+tupleParser = customTupleParser (Pattern " ")
 
 -- | Simple string parser that parses a string of format "xxxx yyyy"
 -- | Takes one Parser function and another function which returns a Parser
@@ -29,8 +29,26 @@ tupleParser parseLeft parseRight = tupleParser' parseLeft (\_ -> parseRight)
 -- |
 -- | This is useful if the value y is dependent on the value x
 tupleParser' :: forall x y. Parser x -> (x -> Parser y) -> String -> Maybe (Tuple x y)
-tupleParser' parseLeft parseRight line = do
-  let parts = split (Pattern " ") line
+tupleParser' = customTupleParser' (Pattern " ")
+
+-- | String parser that parses a string of format "xxxxSSSyyyy"
+-- | Takes two Parser functions and returns a Tuple of both results if
+-- | successful
+-- |
+-- | Additionally takes in a Pattern which provides tha format of the separator SSS
+customTupleParser :: forall x y. Pattern -> Parser x -> Parser y -> String -> Maybe (Tuple x y)
+customTupleParser pattern parseLeft parseRight = customTupleParser' pattern parseLeft (\_ -> parseRight)
+
+-- | String parser that parses a string of format "xxxxSSSyyyy"
+-- | Takes one Parser function and another function which returns a Parser
+-- | based on the value x, then returns a Tuple of both results if successful
+-- |
+-- | Additionally takes in a Pattern which provides tha format of the separator SSS
+-- |
+-- | This is useful if the value y is dependent on the value x
+customTupleParser' :: forall x y. Pattern -> Parser x -> (x -> Parser y) -> String -> Maybe (Tuple x y)
+customTupleParser' pattern parseLeft parseRight line = do
+  let parts = split pattern line
   left <- parts !! 0 >>= parseLeft
   right <- parts !! 1 >>= parseRight left
   Just $ Tuple left right
